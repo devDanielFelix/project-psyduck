66751f6d26907cd48abcd5de7e67a832bc752ced
devDanielFelix <danielfelix72@hotmail.com>
2024-03-20 15:57:51 -0300

    atualização

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -29,7 +29,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Página Inicial</a>
+                        <a class="nav-link" href="/">Página Inicial</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Sobre</a>
